23d8b249a65213e8d0b9b9553b4ef5ae86206a4f
harshuhere01 <harshbavishii@gmail.com>
2022-04-08 00:44:34 +0530

    facebook login

--- lib/auth_class/AuthClass.dart
+++ lib/auth_class/AuthClass.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:jobber/View/Screens/auth_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -8,15 +9,16 @@ import '../Utils/common_utils.dart';
 import '../Utils/navigation_helper.dart';
 import '../View/Screens/dash_board_screen.dart';
 
-class AuthClass{
+class AuthClass {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
   FirebaseAuth auth = FirebaseAuth.instance;
+
   Future<void> googleSignin(BuildContext context) async {
     try {
       GoogleSignInAccount? googleSignInAccount = await _googleSignIn.signIn();
       if (googleSignInAccount != null) {
         GoogleSignInAuthentication googleSignInAuthentication =
-        await googleSignInAccount.authentication;
+            await googleSignInAccount.authentication;
 
         AuthCredential credential = GoogleAuthProvider.credential(
           idToken: googleSignInAuthentication.idToken,
@@ -25,19 +27,17 @@ class AuthClass{
 
         try {
           UserCredential userCredential =
-          await auth.signInWithCredential(credential);
+              await auth.signInWithCredential(credential);
           SharedPreferences prefs = await SharedPreferences.getInstance();
           prefs.setBool('isLogin', true);
           CommonUtils.showGreenToastMessage("Login Successfully");
           await Future.delayed(Duration(milliseconds: 500), () {
-            NavigationHelpers.redirectFromSplash(
-                context, DashBoardScreen());
+            NavigationHelpers.redirectFromSplash(context, DashBoardScreen());
             // CommonUtils.hideDialog(context);
           });
         } catch (e) {
           print(e);
-          CommonUtils.showRedToastMessage(
-              e.toString());
+          CommonUtils.showRedToastMessage(e.toString());
         }
       } else {
         print("google acc is null");
@@ -45,18 +45,55 @@ class AuthClass{
       }
     } catch (e) {
       print(e);
-
     }
   }
 
-  Future<void> signOut(context) async {
+  Future<void> facebookLogin(BuildContext context) async {
+    try{
+      final LoginResult result = await FacebookAuth.instance
+          .login();
 
+      if (result.status == LoginStatus.success) {
+        // you are logged
+        final AccessToken accessToken = result.accessToken!;
+
+        final AuthCredential credential =
+        FacebookAuthProvider.credential(accessToken.token);
+
+        UserCredential credd = await FirebaseAuth.instance
+            .signInWithCredential(credential)
+            .whenComplete(() async {
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          prefs.setBool('isLogin', true);
+          CommonUtils.showGreenToastMessage("Login Successfully");
+          await Future.delayed(Duration(milliseconds: 500), () {
+            NavigationHelpers.redirectFromSplash(context, DashBoardScreen());
+            // CommonUtils.hideDialog(context);
+          });
+        });
+        print("fbuser uid : ${credd.user!.uid}");
+      }
+      if (result.status == LoginStatus.failed) {
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("${result.status}")),
+        );
+      }
+    }
+    catch(e){
+      print('facebook error :-$e');
+    }
+    // by default we request the email and the public profile
+    // or FacebookAuth.i.login()
+
+  }
+
+  Future<void> signOut(context) async {
     try {
       await _googleSignIn.signOut();
       SharedPreferences prefs = await SharedPreferences.getInstance();
       prefs.clear();
       NavigationHelpers.redirectFromSplash(context, AuthScreen());
-
     } catch (e) {
       print('$e + Error while signing out');
     }
